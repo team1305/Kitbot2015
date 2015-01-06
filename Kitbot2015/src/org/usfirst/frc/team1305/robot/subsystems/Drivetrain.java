71a7a2259534c90baa3485cbd4f468f3eba18336
paul <paulirino@gmail.com>
2015-01-05 21:02:12 -0500

    updated port numbers

--- Kitbot2015/src/org/usfirst/frc/team1305/robot/subsystems/Drivetrain.java
+++ Kitbot2015/src/org/usfirst/frc/team1305/robot/subsystems/Drivetrain.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team1305.robot.subsystems;
 
+import org.usfirst.frc.team1305.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -11,15 +13,16 @@ public class Drivetrain extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	private Talon mL1 = new Talon(0);
-	private Talon mL2 = new Talon(0);
-	private Talon mR1 = new Talon(0);
-	private Talon mR2 = new Talon(0);
+	private Talon mL1 = new Talon(RobotMap.PWM_Drive_Back_Left);
+	private Talon mL2 = new Talon(RobotMap.PWM_Drive_Front_Left);
+	private Talon mR1 = new Talon(RobotMap.PWM_Drive_Back_Right);
+	private Talon mR2 = new Talon(RobotMap.PWM_Drive_Front_Right);
 	private RobotDrive drive = new RobotDrive(mL1, mL2, mR1, mR2);
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand();
     }
     
     public void ArcadeDrive(double moveValue, double rotateValue){
